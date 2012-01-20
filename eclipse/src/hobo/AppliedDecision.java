5f3007d2f4d944fddbf6d30ebbe33094300cd491
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2012-01-20 23:07:16 +0100

    more fail-fast in undo system

--- eclipse/src/hobo/AppliedDecision.java
+++ eclipse/src/hobo/AppliedDecision.java
@@ -6,16 +6,21 @@ public abstract class AppliedDecision {
 	protected State state; // state to which we were applied
 	protected MersenneTwisterFast old_random; // random state before
 	protected int old_player; // current player before
-	
+	protected boolean undone;
+
 	public AppliedDecision(Decision decision, State state) {
 		this.decision = decision;
 		this.state = state;
 		this.old_random = state.random.clone();
 		this.old_player = state.currentPlayer();
+		this.undone = false;
 	}
-	
+
 	public void undo() {
-		state.random = old_random.clone();
+		if (undone)
+			throw new RuntimeException();
+		state.random = old_random;
 		state.switchToPlayer(old_player);
+		undone = true;
 	}
 }
